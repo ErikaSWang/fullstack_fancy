fba57bfd64f3da928d9e1b22c276dca010bb3da9
Erika Wang <ErikaWang@gmail.com>
2026-02-09 19:17:03 -0500

    vercel not working :( Rolling back vite.config.js as well, to try to get it to work

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5000,
-    allowedHosts: ['all', process.env.REPLIT_DEV_DOMAIN, '.replit.dev'].filter(Boolean),
+    allowedHosts: ['all'],
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
