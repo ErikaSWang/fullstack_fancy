36ecdd888cca1f3d3c753814d8dc5a4c17f49ed3
Erika Wang <ErikaWang@gmail.com>
2026-02-14 17:39:45 -0500

    Added morgan, added helmet

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import cors from 'cors'
-import loadingRouter from './routes/loading.js';
+import welcomeRouter from './routes/welcome.js';
 import errorMessagesRouter from './routes/errorMessages.js';
 
+
+
+// SETTING UP THE SERVER
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -10,11 +13,21 @@ app.use(cors())
 app.use(express.json())
 
 
+
+// LOGGING MIDDLEWARE 
+import morgan from 'morgan'
+
+app.use(morgan('dev')) // Logs all incoming requests to the terminal console (nothing shows without it)
+
+
+
+
+
 // ACTUAL ROUTES HERE
 // API routes must come before static file serving
 
 // 'Mounting' the welcome router
-app.use('/api', loadingRouter);
+app.use('/api', welcomeRouter);
 
 app.get('/api/hello', (req, res) => {
   res.status(200).json(
@@ -28,7 +41,18 @@ app.get('/api/hello', (req, res) => {
 app.use('/api', errorMessagesRouter);
 
 
-// For Vercel AND Render - builds a production mode version of the app
+// HTTP HEADER SECURITY MIDDLEWARE
+import helmet from 'helmet'
+
+app.use(helmet()) // Adds security-related HTTP headers to protect against common vulnerabilities (e.g., XSS, clickjacking, etc.)
+
+
+
+
+
+
+// PRODUCTION BUILD - SERVE REACT APP
+// For Vercel and Render - builds a production mode version of the app
 
 import path from 'path'
 import { fileURLToPath } from 'url'
